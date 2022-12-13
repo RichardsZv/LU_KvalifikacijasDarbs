87590f33e2c4b42444536548dc0c9f994a34eaa5
RichardsZv <richards.zvagulis@gmail.com>
2022-12-13 23:38:12 +0200

    Trenera skata audzēkņa treniņu izpildes tabula.

--- DataAccessLibrary/Models/CoachReportDataModel.cs
+++ DataAccessLibrary/Models/CoachReportDataModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace DataAccessLibrary.Models
 {
-    internal class CoachReportDataModel
+    public class CoachReportDataModel
     {
+        public int Id { get; set; }
+        public float Km { get; set; }
+        public int Time { get; set; } //Laiks Minūtēs
+        public string Completed { get; set; }
+        public TimeSpan Pace { get; set; }
+        public int Pulse { get; set; }
+        public int Vert { get; set; }
+        public float E { get; set; }
+        public float S { get; set; }
+        public float R { get; set; }
+        public float I { get; set; }
+        public float T { get; set; }
+        public float M { get; set; }
+        public float VDOT { get; set; }
+     
     }
 }
