4afc2febfc495fd21f824e2c28c90d90cb7d5368
RichardsZv <richards.zvagulis@gmail.com>
2022-12-15 23:37:10 +0200

    Audzēkņu treniņu dienasgrāmatas attēlošana trenerim.

--- DataAccessLibrary/Models/ReportWeekModel.cs
+++ DataAccessLibrary/Models/ReportWeekModel.cs
@@ -10,8 +10,8 @@ namespace DataAccessLibrary.Models
     {
         public int Id { get; set; }
         public int ReportId{ get; set; }
-        public DateTime DatS { get; set; }
-        public DateTime DatB { get; set; }
+        public DateTime Dat_S { get; set; }
+        public DateTime Dat_B { get; set; }
         public int WeekCount { get; set; }
         public int Week_Num { get; set; }
     }
--- SupervaroniApp/Program.cs
+++ SupervaroniApp/Program.cs
@@ -71,11 +71,11 @@ builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuth
 builder.Services.AddMudServices();
 builder.Services.AddMudServices(config =>
 {
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
     config.SnackbarConfiguration.PreventDuplicates = false;
-    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.NewestOnTop = true;
     config.SnackbarConfiguration.ShowCloseIcon = true;
-    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
     config.SnackbarConfiguration.HideTransitionDuration = 500;
     config.SnackbarConfiguration.ShowTransitionDuration = 500;
     config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
