e2941afe7b1bef69c17ce277a2930e277a899aae
RichardsZv <richards.zvagulis@gmail.com>
2022-12-20 13:34:54 +0200

    -UI izmaiņas -Iesākta tabulu datu validācija

--- DataAccessLibrary/Models/ReportDataPlannedModel.cs
+++ DataAccessLibrary/Models/ReportDataPlannedModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace DataAccessLibrary.Models
 {
     public class ReportDataPlannedModel
     {
+        [Key]
         public int Id { get; set; }
         public DateTime Dat { get; set; }
         public int Report_Week_Id { get; set; }
+        [StringLength(150, ErrorMessage = "Plāna apraksta garums pārsniedz atļauto limitu")]
         public string Plan_Description { get; set; }
     }
 }
